37b369220b2b0cabddebce7475769a4afc1ad44b
Rawatkhushi <rawatkhushi004@gmail.com>
2024-10-20 14:07:22 +0530

    initial commit

---
+++ 01-basics.js
@@ -0,0 +1,12 @@
+const a = 13;
+let b = "khushi";
+var c = "123";
+account = "helloooo";
+
+// a=3 will throw error, not allowed
+b = "rawat";
+c = "456";
+account = "worldd";
+
+console.log("this can work separately");
+console.table([a, b, c, account]);
