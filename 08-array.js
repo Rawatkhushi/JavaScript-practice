1f946ed9e0ae1fbd00fd971ebd89db2cc8c1156d
Rawatkhushi <rawatkhushi004@gmail.com>
2024-11-12 12:03:18 +0530

    arrays part-2

--- 08-array.js
+++ 08-array.js
@@ -28,3 +28,45 @@ console.log("A", myarr);
 
 const a = myarr.slice(1, 4);
 console.log(a);
+console.log(myarr);
+
+console.log(myarr.splice(0, 3));
+console.log(myarr.splice(1, 0, "khushi")); //adds khushi to 1 index replacing 0 elements
+console.log(myarr);
+
+console.log(myarr.splice(2, 1, "rawat")); //replaces 1 element at 2 index with rawat
+console.log(myarr);
+
+console.log(myarr.splice(3, 1, "happy", "happy")); //replaces 1 element at 2nd index with rawat happy
+console.log(myarr);
+
+//note- slice cuts out a portion of array(exluding the last range) keeping the original array unaffected but splice affects the original array and prints out the elements being removed or replaced
+
+//Arrays-2
+
+const arr = ["pretty", "nice"];
+myarr.push(arr);
+console.log(myarr); //[ 1, 2, 3, 4, 5, 6, 7, [ 'pretty', 'nice' ] ] treats arr as single element
+
+const arr2 = myarr.concat(arr);
+console.log(arr2); //[ 1, 2, 3, 4, 5, 6, 7, 'pretty', 'nice' ] adds arrays and store it in a new variable
+
+const bc = [...myarr, ...arr]; //SPREAD OPERATOR
+console.log(bc); //[ 1, 2, 3, 4, 5, 6, 7, 'pretty', 'nice' ] same thing without concat
+
+console.log(...myarr, ...arr); //1 2 3 4 5 6 7 pretty nice
+
+const cc = [1, 2, 3, [4, 5], 6, [7, 8, [9, 10, 11], 12]];
+console.log(cc.flat(Infinity)); //[1,  2, 3, 4,  5, 6,  7, 8, 9, 10, 11, 12] converts multi d array into single d array
+console.log(cc.flat(1)); //only converts upto 1 degree
+
+console.log(Array.isArray(myarr)); //true-returns true if it is an array
+console.log(Array.from("khushi")); //[ 'k', 'h', 'u', 's', 'h', 'i' ] converts passed parameter into array
+
+console.log(Array.from({ name: "khushi" })); //[]
+
+let ab = 100;
+let b = 200;
+let c = 300;
+
+console.log(Array.of(ab, b, c)); //[ 100, 200, 300 ]
