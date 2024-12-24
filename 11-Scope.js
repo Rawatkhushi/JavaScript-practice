4a168e889efcf78d668a9d0d5ea72cc560c95756
Rawatkhushi <rawatkhushi004@gmail.com>
2024-12-24 13:16:40 +0530

    Scope level and Mini Hoisting

--- 11-Scope.js
+++ 11-Scope.js
@@ -15,3 +15,39 @@ if (true) {
   console.log(ab); //100
 }
 console.log(ab); //1
+
+//scope level and mini hoisting
+function one() {
+  const name = "vanshika";
+  function two() {
+    const age = 20;
+    console.log(name);
+  }
+
+  //   console.log(age);//age is not defined because the scpe of age has ended
+
+  two();
+}
+
+one(); //-->vanshika
+
+//same scope concept can be extended to if-else as well
+
+-----------hoisting intro-----------
+
+
+//method-1 for functions
+
+console.log(add1(4));
+function add1(num) {
+  return num + 1;
+}
+
+//method-2 for functions or an "expression"
+
+console.log(add2(4)); //error: Cannot access 'add2' before initialization
+
+const add2 = function (num) {
+  return num + 2;
+};
+
