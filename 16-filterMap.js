b87a545b3a22b6e9ac9c24fee2003ec78aecc069
Rawatkhushi <rawatkhushi004@gmail.com>
2025-03-19 22:51:11 +0530

    map and reduce

--- 16-filterMap.js
+++ 16-filterMap.js
@@ -1,15 +1,15 @@
-// const coding = ["khushi", "rawat"];
-// const val = coding.forEach((item) => {
-//   console.log(item);
-//   return item;
-// });
-// console.log(val); //undefined
-// //can't store values
+const coding = ["khushi", "rawat"];
+const val = coding.forEach((item) => {
+  console.log(item);
+  return item;
+});
+console.log(val); //undefined
+//can't store values
 
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const mynum = nums.filter((key) => key > 4); //a callback function(fxn with no name) => condition on which it filters
-// console.log(mynum); //[ 5, 6, 7, 8, 9 ]
-// //the value gets stores in mynum
+const mynum = nums.filter((key) => key > 4); //a callback function(fxn with no name) => condition on which it filters
+console.log(mynum); //[ 5, 6, 7, 8, 9 ]
+//the value gets stores in mynum
 
 //iterating while condition checking in forEach - alternative for filter
 const newnums = [];
@@ -74,3 +74,59 @@ console.log(newbooks1);
 //     { title: 'three', genre: 'science', publish: '2013' },
 //     { title: 'six', genre: 'science', publish: '2016' }
 // ]
+
+//map method
+const newarray = [1, 2, 3, 4, 5, 6];
+const superarray = newarray.map((item) => {
+  return item * 2;
+});
+console.log(superarray); //[ 2, 4, 6, 8, 10, 12 ]
+//map- modifies each element and return an array with no. of elements equal to that of previous one
+//filter - filters the array on a condition ,no. of elements equal to or lesser than prev one
+//forEach -  doesn't return an array like map so manual creation of array and pushing of element is required
+
+//same example on forEach
+let superarray1 = [];
+newarray.forEach((item) => {
+  superarray1.push(item * 2);
+});
+console.log(superarray1); //[ 2, 4, 6, 8, 10, 12 ]
+
+//reduce on array (arrow function)
+const mynums = [1, 2, 3, 4];
+const total = mynums.reduce((acc, currval) => {
+  console.log(`value of acc: ${acc}, val of currval: ${currval}`);
+
+  return acc + currval;
+}, 10);
+console.log(total);
+
+//total of shopping cart example
+const shoppingCart = [
+  {
+    itemname: "shoes",
+    price: 10000,
+  },
+  {
+    itemname: "socks",
+    price: 500,
+  },
+  {
+    itemname: "top",
+    price: 700,
+  },
+  {
+    itemname: "pants",
+    price: 1200,
+  },
+  {
+    itemname: "gloves",
+    price: 300,
+  },
+];
+
+const total1 = shoppingCart.reduce((acc, currval) => {
+  return acc + currval.price;
+}, 0);
+
+console.log(total1);
